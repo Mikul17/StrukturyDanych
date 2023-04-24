a848a604094eec92fda6c524141bbc9638d44459
Mikul17 <84858817+Mikul17@users.noreply.github.com>
2023-04-24 17:00:16 +0200

    adding hashmap implementation (replacing hashset)

---
+++ src/main/java/org/example/Hashmap.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Hashmap<T>{
+    //variables
+
+
+    //constructor
+
+    //put methods
+
+    //get methods
+
+    //remove methods
+
+    //contains methods
+
+    //size method
+}
